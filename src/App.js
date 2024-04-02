e8ba871ff51cb453c7629f609f1f6d6cdb158f76
Linasenbye <elyneseh@gmail.com>
2024-04-02 17:19:13 +0900

    working on the buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
-import ClassCounter from "./components/classCounter";
+import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/input/button/MyButton";
 
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState('Text in the Input');
-  
-  
+  const [posts, setPosts] = useState([
+    {id:1, title:'JavaScript 1', body:'Description'}, 
+    {id:2, title:'JavaScript 2', body:'Description'}, 
+    {id:3, title:'JavaScript 3', body:'Description'}
+  ])  
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <form>
+        <input type="text" placeholder="Post Title"/>
+        <input type="text" placeholder="Description of the Post"/>
+        <MyButton>Make a post</MyButton>
+      </form>
+      <PostList posts={posts} title="List of posts 1"/>
     </div>
   );
 }
